ecdc65dd9bdb68ff196f6d783a1bf979b5252211
Brian <you@example.com>
2020-10-26 16:21:11 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { React, Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    families: [
+      "Julie & Caleb",
+      "Mary & Brian",
+      "Allen & Crystal",
+      "David & Laurel",
+      "Wendy & Ty",
+      "Laura & Matt",
+      "Mom & Dad",
+    ],
+    currentlyServed: "",
+  };
+
+  getRandom() {
+    let num = Math.floor(Math.random() * this.state.families.length);
+    let newState = this.state.families;
+    const currentlyServed = newState.splice(num, 1);
+    return this.setState({
+      families: newState,
+      currentlyServed,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="randomizer">
+          <span>Family Randomizer 3000</span>
+
+          <button onClick={() => this.getRandom()} className="shuffle">
+            Shuffle!
+          </button>
+          <div className="title">Family being served:</div>
+          <div className="serving">
+            {!this.state.currentlyServed
+              ? "click shuffle!"
+              : this.state.currentlyServed}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
